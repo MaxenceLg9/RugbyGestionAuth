967899a8aa3becf0516eac5aa6aba79e988b0ade
KOH Virgil Shaun <virgil-shaun.koh@etu.iut-tlse3.fr>
2025-03-05 15:22:53 +0100

    [CODE] set up table user and data for authentification

---
+++ bdUser.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Dim 25 Février 2024 à 19:44
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `projetapi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- insérer des données dans la table `user`
+INSERT INTO `user` (`email`, `password`, `role`) VALUES
+('maxenceL9@gmail.com', '$2y$10$PsJZvmmnvbGwIYDBeEGtC.Z1Bkulg1W1eyhOtPsjPSL4DJ72D97xy', 'admin'),
+('virgilskoh@gmail.com', '$2y$10$cefdlQN3gWXEo7AseLD4q.GoF3LH73CaD.b5nhmeIGf8aYNMJ8Mje', 'admin');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
